0e8f855e5cf82a8fe3952798ad4d22a9dcaf5db1
aveA <aveA@AVE>
2025-05-20 22:48:18 +0300

    objects and window.sessionStorage

--- LifeSpot/Static/Scripts/index.js
+++ LifeSpot/Static/Scripts/index.js
@@ -1,22 +1,14 @@
-﻿let session = new Map();
+﻿//let session = {
+//    userAgent: window.navigator.userAgent,
+//    startDate: new Date().toLocaleString(),
+//    userAge: prompt("Укажите сколько вам лет")
+//}
 
-function handleSession() {
+function checkAge(newVisit) {
 
-    let userAgent = window.navigator.userAgent;
-    session.set('userAgent', userAgent);
-
-    let startDate = new Date().toLocaleString();
-    session.set('startDate', startDate);
-
-}
-
-function checkAge() {
-
-    let age = prompt("Укажите сколько вам лет");
-    session.set('age', age);
-
-    if (age >= 18) {
-        alert("Приветствуем на LifeSpot. Время входа:\n" + new Date().toLocaleString());
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        if(newVisit)
+            alert("Приветствуем на LifeSpot. Время входа:\n" + session.age);
     }
     else {
         alert("Сайт предназначен для лиц старше 18 лет");
@@ -27,15 +19,41 @@ function checkAge() {
 
 let sessionLog = function logSession() {
 
-    for (let result of session) {
-        console.log(result)
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: : ' + window.sessionStorage.getItem("userAge"))
+}
+
+function handleSession(logger, checker) {
+
+    // Проверяем дату захода и проставляем, если новый визит
+    if (window.sessionStorage.getItem("startDate") == null)
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString());
+
+    // Проверяем userAgent и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAgent") == null)
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent);
+
+    // Проверяем возраст и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAge") == null) {
+
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        // Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и при прохождении проверки на возраст он увидит приветствие
+        checker(true)
+    } else {
+
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
     }
 
+    // Вызываем переданную в качестве колл-бэка функцию логирования.
+    logger()
 }
 
 function filtredInput(element) {
 
-    //let enteredText = document.getElementsByTagName('input')[0].value.toLowerCase();
     let enteredText = element.value.toLowerCase();
 
     let videoElements = document.getElementsByClassName('video-container');
