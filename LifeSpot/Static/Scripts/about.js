fcfd5b46a088753869224300fc2dc5ba2417ac12
aveA <aveA@AVE>
2025-05-21 23:42:13 +0300

    prototype __proto__

--- LifeSpot/Static/Scripts/about.js
+++ LifeSpot/Static/Scripts/about.js
@@ -1,5 +1,4 @@
-﻿
-function getReview() {
+﻿function getReviewOld() {
 
     let review = {};
 
@@ -16,8 +15,41 @@ function getReview() {
     setReview(review);
 }
 
+function getReview() {
+
+    let review = {};
+
+    review.userName = prompt('Введите Ваше имя:');
+    if (review.userName == null)
+        return;
+
+    review.comment = prompt('Напишите отзыв:');
+    if (review.comment == null)
+        return;
+
+    review.date = new Date().toLocaleString();
+
+    let isLikable = confirm("Хоите, чтобы Ваш отзыв могли оценить другие?")
+
+    if (isLikable) {
+
+        let reviewWithFeedback = Object.create(review);
+        reviewWithFeedback.rate = 0;
+
+        setReview(reviewWithFeedback);
+    } else {
+        setReview(review)
+    }
+}
+
 const setReview = review => {
 
+    let likeCounter = '';
+
+    if (review.hasOwnProperty('rate')) {
+        likeCounter += `<b style="color: chocolate">Рейтинг:${review.rate}</b>`;
+    }
+
     document.getElementsByClassName('reviews')[0].innerHTML +=
-        `<div class=\"review-text\"><p><b>${review.userName}</b>  ${review.date}</p><p>${review.comment}</p></div>`;
+        `<div class=\"review-text\"><p><b>${review.userName}</b>  ${review.date} ${likeCounter}</p><p>${review.comment}</p></div>`;
 }
