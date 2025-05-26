18daf76c24f13ef0f4c3e7fbab40e491c8765a54
aveA <aveA@AVE>
2025-05-26 23:45:01 +0300

    +

--- LifeSpot/Static/Scripts/about.js
+++ LifeSpot/Static/Scripts/about.js
@@ -15,7 +15,7 @@
     setReview(review);
 }
 
-function getReview() {
+function getReviewOld2() {
 
     let review = {};
 
@@ -42,12 +42,76 @@ function getReview() {
     }
 }
 
+function Comment() {
+    this.userName = prompt('Введите Ваше имя:');
+    if (this.userName == null) {
+        this.empty = true;
+        return;
+    }
+
+    this.comment = prompt('Напишите отзыв:');
+    if (this.comment == null) {
+        this.empty = true;
+        return;
+    }
+
+    this.date = new Date().toLocaleString();
+}
+
+function addComment() {
+    let comment = new Comment()
+
+    if (comment.empty) {
+        return;
+    }
+
+    let isLikable = confirm("Хоите, чтобы Ваш отзыв могли оценить другие?")
+
+    if (isLikable) {
+
+        let reviewWithFeedback = Object.create(comment);
+        reviewWithFeedback.rate = 0;
+
+        setReview(reviewWithFeedback);
+    } else {
+        setReview(comment)
+    }
+}
+
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1;
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`;
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ');
+}
+
 const setReview = review => {
 
     let likeCounter = '';
 
     if (review.hasOwnProperty('rate')) {
-        likeCounter += `<b style="color: chocolate">Рейтинг:${review.rate}</b>`;
+
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, 
+        // и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
     }
 
     document.getElementsByClassName('reviews')[0].innerHTML +=
--- LifeSpot/Static/Scripts/testing.js
+++ LifeSpot/Static/Scripts/testing.js
@@ -1,2 +1,12 @@
 ﻿let elements = document.getElementsByTagName('*');
 alert(`Количество элементов на странице:  ${elements.length}`);
+
+function saveInputHistory() {
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    //равноценно window.lastInput, так как мы работаем в контексте браузера
+    alert('Последний ввод: ' + this.lastInput + '\n' + 'Текущий ввод: ' + currentInput);
+
+    this.lastInput = currentInput;
+}
